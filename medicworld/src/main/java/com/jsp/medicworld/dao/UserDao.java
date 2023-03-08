3339cc5760ba616c2e034cd8dfd162d2ecb2a961
Sagar <sagar.a.ingale@gmail.com>
2023-03-08 17:19:22 +0530

    done

--- medicworld/src/main/java/com/jsp/medicworld/dao/UserDao.java
+++ medicworld/src/main/java/com/jsp/medicworld/dao/UserDao.java
@@ -1,5 +1,7 @@
 package com.jsp.medicworld.dao;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -15,4 +17,10 @@ public interface UserDao extends JpaRepository<User, Integer> {
 	User findByEmailAndPhone(String username, long phone);
 
 	User findByEmailOrPhone(String email,long phone);
+	
+	@Query("SELECT u FROM User u WHERE u.username=:query OR u.email=:query")
+	User findUser(String query);
+	Optional<User> findByemail(String email);
+
+	
 }
--- medicworld/src/main/java/com/jsp/medicworld/service/UserService.java
+++ medicworld/src/main/java/com/jsp/medicworld/service/UserService.java
@@ -4,12 +4,16 @@ import com.jsp.medicworld.entity.User;
 
 
 
-public interface UserService {
+public interface UserService  {
 	
 	Object signupUser(User u);
 	
 	Object loginUser(String email,String password);
 	
 	Object getAllUser();
+	
+	public String findByemail(String email);
+	
+	public String changePwdService(String password,String confirm);
 
 }
